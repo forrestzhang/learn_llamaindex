4e58a3fdf4b7eed92243196eb4458e858c2dd29e
Tao Zhang <forrest_zhang@163.com>
2024-08-01 15:28:42 +0800

    add duckduckgo

--- 5_Agents/3_tool_agent.py
+++ 5_Agents/3_tool_agent.py
@@ -4,13 +4,14 @@ from llama_index.core.tools import FunctionTool
 from llama_index.core import Settings
 from llama_index.tools.yahoo_finance import YahooFinanceToolSpec
 from llama_index.tools.wikipedia import WikipediaToolSpec
+from llama_index.tools.duckduckgo import DuckDuckGoSearchToolSpec
 
 # 可以从 https://llamahub.ai/ 找到合适的tool
 """
 pip install llama-index-tools-yahoo-finance
 pip install llama-index-tools-wikipedia 
 """
-Settings.llm = Ollama(model="llama3")
+Settings.llm = Ollama(model="llama3.1")
 
 # function tools
 def multiply(a: float, b: float) -> float:
@@ -26,22 +27,33 @@ def add(a: float, b: float) -> float:
 add_tool = FunctionTool.from_defaults(fn=add)
 
 # Yahoo Finance tool
-print("----------------------Yahoo Finance Tool----------------------")
+print("\n\n----------------------Yahoo Finance Tool----------------------")
 
 finance_tools = YahooFinanceToolSpec().to_tool_list()
 finance_tools.extend([multiply_tool, add_tool])
 
-yahoo_fiance_agent = ReActAgent.from_tools(finance_tools, verbose=True, max_iterations=30)
+yahoo_fiance_agent = ReActAgent.from_tools(finance_tools, verbose=False, max_iterations=100)
 
 response = yahoo_fiance_agent.chat("What is the current price of NVDA?")
+print("Q: What is the current price of NVDA?")
 
 print(response)
 
 # Wikipedia tool
-print("----------------------Wikipedia Tool----------------------")
+print("\n\n----------------------Wikipedia Tool----------------------")
 wiki_tools = WikipediaToolSpec().to_tool_list()
-wiki_agent = ReActAgent.from_tools(wiki_tools, verbose=True, max_iterations=30)
+wiki_agent = ReActAgent.from_tools(wiki_tools, verbose=False, max_iterations=100)
 
 response = wiki_agent.chat("What is the capital of France?")
+print("Q: What is the capital of France?")
 print(response)
 
+
+# DuckDuckGo Search tool
+print("\n\n----------------------DuckDuckGo Search Tool----------------------")
+search_tools = DuckDuckGoSearchToolSpec().to_tool_list()
+search_agent = ReActAgent.from_tools(search_tools, verbose=False, max_iterations=100)
+
+response = search_agent.chat("which country host olympic 2024?")
+print("Q: which country host olympic 2024?")
+print(response)
